6a3326842b66dfeb94e77a023929a015d45f7cb0
Freda Suyi Ding <freda.ding@wework.com>
2017-10-07 22:12:57 -0400

    add session token, login, logout methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,18 @@
 class SessionController < ApplicationController
   def create
-    @user
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login @user
+    else
+      render json: ['Invalid username or password'], status: 401
+    end
   end
 
   def destroy
-
+    logout
+    render json: {}
   end
 end
