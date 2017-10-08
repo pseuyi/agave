b6f6f76d0efc19419dd4fcc53a17eb81d61d0883
Freda Suyi Ding <freda.ding@wework.com>
2017-10-08 03:16:44 -0400

    nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   constraints format: :json do
     resources :session, only: [:create, :destroy]
-    resources :users, only: [:show, :create, :update]
-    resources :tasks, only: [:list, :show, :create, :update, :destroy]
+    resources :users, only: [:show, :create, :update] do
+      resources :tasks, only: [:list, :show, :create, :update, :destroy]
+    end
   end
 end
