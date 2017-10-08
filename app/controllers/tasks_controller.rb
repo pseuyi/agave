a1b7aa769ec70d17df317bf065a41541c8cd7552
Freda Suyi Ding <freda.ding@wework.com>
2017-10-08 03:10:41 -0400

    add task methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  def list
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: @task
+  end
+
+  def create
+    @user = current_user
+    @task = @user.tasks.create(task_params)
+    if @task.save
+      redirect_to :action => 'list'
+    else
+      render json: @task.errors.full_messages, status: 422
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to :action => 'list'
+    else
+      render json: @task.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @task = @user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to :action => 'list'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :priority)
+  end
+
 end
