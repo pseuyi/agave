372c91dec5014cc7eb6a6b129c841cbcd23de966
Freda Suyi Ding <freda.ding@wework.com>
2017-10-08 01:25:59 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require `faker`
-
 10.times do
   username = Faker::Internet.user_name
   email = Faker::Internet.email
-  User.create({ username: username, email: email })
-
+  password = 'carrot'
+  User.create({ username: username, email: email, password: password })
 end
