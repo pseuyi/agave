e9c0b0cae789f73d94b8a1e5c22e330f7ff7ffe6
Freda Suyi Ding <freda.ding@wework.com>
2017-10-07 21:56:48 -0400

    update user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :password, :email)
   end
 end
