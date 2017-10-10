5d57e2cccf8d75f95cfdf7bbc0ad949de572323f
Freda Suyi Ding <freda.ding@wework.com>
2017-10-09 23:24:50 -0400

    update styles

--- frontend/components/header/index.jsx
+++ frontend/components/header/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <div className="logo-box">
         <h2 id="logo">agave</h2>
-        <div className="logo-button">
-          <Link to="/login">login</Link>
-          <Link to="/signup">signup</Link>
-        </div>
       </div>
     </header>
   )
