b9360bc29f6826f2dd9b697c3e20fdb4e0d97670
Calvin McElroy <fourwallsstudio@gmail.com>
2017-10-15 20:19:49 -0400

    move preload state to util

--- frontend/agave.jsx
+++ frontend/agave.jsx
@@ -2,26 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import { getPreloadedState } from './util/session_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  let preloadedState;
-
-  if (localStorage['currentUser']) {
-    const data = JSON.parse(localStorage['currentUser']);
-
-    preloadedState = {
-      session: {
-        currentUser: parseInt(data.id)
-      },
-      users: {
-        usersByIds: {
-          [data.id]: data.attributes
-        }
-      }
-    }
-  }
-
+  const preloadedState = getPreloadedState();
   const store = configureStore(preloadedState);
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
--- frontend/util/session_util.js
+++ frontend/util/session_util.js
@@ -6,3 +6,23 @@ export const setUserLocalStorage = (user) => {
 export const removeUserLocalStorage = () => {
   delete localStorage['currentUser'];
 }
+
+
+export const getPreloadedState = () => {
+  if (localStorage['currentUser']) {
+    const data = JSON.parse(localStorage['currentUser']);
+
+    return {
+      session: {
+        currentUser: parseInt(data.id)
+      },
+      users: {
+        usersByIds: {
+          [data.id]: data.attributes
+        }
+      }
+    };
+  } else {
+    return null;
+  }
+}
