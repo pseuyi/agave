4ed81fb0705ad3ef0b77364b9cde93db6ebcb62f
Freda Suyi Ding <freda.ding@wework.com>
2017-11-11 17:13:55 -0500

    update footer

--- frontend/components/footer/index.jsx
+++ frontend/components/footer/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import TaskForm from 'components/task_form';
 import style from './index.scss';
 
 const Footer = (props) => {
   return (
     <footer className="footer">
-      
+      <TaskForm handleSubmit={() => {}}/>
     </footer>
   )
 }
-
 
 export default Footer;
