1de98f5ddb7d7f886e4940797e8e047a31a9e922
Freda Suyi Ding <freda.ding@wework.com>
2017-11-12 18:09:35 -0500

    add login to landing

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SessionModal from 'components/session_modal';
 
 const Landing = () => (
-  [
-    <h1>WELCOM TO AGAVE</h1>,
-    <SessionModal />
-  ]
+    <section>
+      <h1>WELCOM TO AGAVE</h1>
+      <Link to="/login">LOGIN</Link>
+    </section>
 )
 
 export default Landing;
