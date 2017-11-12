4cc7fe117d626b73584fac01eb0b20c46d75ac50
Freda Suyi Ding <freda.ding@wework.com>
2017-11-12 17:55:31 -0500

    create landing and home components

---
+++ frontend/components/landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SessionModal from 'components/session_modal';
+
+const Landing = () => (
+  [
+    <h1>WELCOM TO AGAVE</h1>,
+    <SessionModal />
+  ]
+)
+
+export default Landing;
