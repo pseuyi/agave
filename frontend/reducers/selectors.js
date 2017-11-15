a151d7a57bcaf02f9f217b9dad22af55dedeafb1
Calvin McElroy <fourwallsstudio@gmail.com>
2017-11-15 00:42:10 -0500

    remove unused getLastPriority

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -26,11 +26,3 @@ export const newPrioritySelector = createSelector(
     return filter(tasks, (task) => task.status === status ).length + 1;
   }
 )
-
-export const getLastPriority = (status, tasks) => {
-  let priority = 1
-  tasks.forEach(task => {
-    if (task.status === status) priority += 1
-  })
-  return priority;
-}
