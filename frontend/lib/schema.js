1d53a9eb3a8ca8d1f395398817f7dab805d70871
Calvin McElroy <fourwallsstudio@gmail.com>
2017-11-16 17:16:17 -0500

    add schema comment

--- frontend/lib/schema.js
+++ frontend/lib/schema.js
@@ -2,3 +2,17 @@ import { schema } from 'normalizr';
 
 export const task = new schema.Entity('tasks');
 export const tasks = { tasks: [ task ] }
+/*
+  input:
+  {
+    tasks: [{id: 1, title: 'sdf'}, {id: 2, title: 'sdasad'}]
+  }
+
+  outcome:
+  {
+    result: [1, 2],
+    entities: {
+      tasks: { 1: {}, 2: {} }
+    }
+  }
+*/
