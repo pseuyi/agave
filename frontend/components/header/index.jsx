114248a9c0e9850d5b2bdf952840746a16883d68
Freda Suyi Ding <freda.ding@wework.com>
2017-11-19 00:43:58 -0500

    update Header styles

--- frontend/components/header/index.jsx
+++ frontend/components/header/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { currentUserSelector } from 'reducers/selectors';
 import { Link } from 'react-router-dom';
 import { logout } from 'actions/session_actions';
+
+const Container = styled.header`
+  height: 100px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: safe center;
+`
+const LogoContainer = styled.div`
+  padding: 0 4.4rem;
+`
+const Logo = styled.a`
+  font-size: 6rem;
+  color: #1bd68e;
+  font-weight: lighter;
+  font-family: 'BodoniXt';
+  letter-spacing: 10px;
+`
+const UserContainer = styled.div`
+  padding: 0 4.4rem;
+  font-size: 1.4rem;
+`
+const LeftNav = styled.div`
+  display: block;
+  top: 0;
+  h4 {
+    margin: 0.4rem 0;
+  }
+`
 
 const Header = (props) => {
 
@@ -12,23 +42,26 @@ const Header = (props) => {
   }
 
   return (
-    <header>
-      <div className='user-box'>
+    <Container>
+      <UserContainer>
         {
           props.currentUser ?
-            [
-              <h4 key='username'>{props.currentUser.username}</h4>,
-              <button key='logout' onClick={handleLogout}>logout</button>
-            ] : [
+            (
+              <LeftNav>
+                <h4 key='username'>welcome, {props.currentUser.username}</h4>
+                <button key='logout' onClick={handleLogout}>logout</button>
+              </LeftNav>
+            )
+             : [
               <Link key='login' to='/login'>login</Link>,
               <Link key='signup' to='/signup'>sign up</Link>
             ]
         }
-      </div>
-      <div className='logo-box'>
-        <h2 id='logo'>agave</h2>
-      </div>
-    </header>
+      </UserContainer>
+      <LogoContainer>
+        <Logo href="/">agave</Logo>
+      </LogoContainer>
+    </Container>
   )
 }
 
