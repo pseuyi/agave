ac231cbf493b11b398f0a5a5b3a1254569e1dd59
Calvin McElroy <fourwallsstudio@gmail.com>
2017-11-21 00:10:11 -0500

    prep heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171008221527) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
