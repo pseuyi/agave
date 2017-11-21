07184d0f5f2e6be359f71566d5e27ab478e04d4f
Freda Suyi Ding <freda.ding@wework.com>
2017-11-21 15:11:30 -0800

    add demo login

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import SessionModal from 'components/session_modal';
+import { login } from 'actions/session_actions';
 
 const Modal = styled.section`
   position: absolute;
@@ -34,20 +35,38 @@ const Links = styled.div`
   color: #1bd68e;
 `
 
-const Landing = ({ loggedIn, history }) => {
-  if (loggedIn) history.push("/home")
-  return (
-    <Modal>
-      <h1>agave</h1>
-      <Links>
-        <Link to="/login">login</Link>/<Link to="/signup">signup</Link>
-      </Links>
-    </Modal>
-  )
+class Landing extends Component {
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const data = {
+      username: 'garry',
+      password: 'carrot'
+    };
+    this.props.demoLogin(data);
+  }
+
+  render () {
+    const { loggedIn, history } = this.props;
+    if (loggedIn) history.push("/home");
+    return (
+      <Modal>
+        <h1>agave</h1>
+        <Links>
+          <Link to="/login">login</Link>/<Link to="/signup">signup</Link>
+          <p><button onClick={this.handleClick}>demo</button></p>
+        </Links>
+      </Modal>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
-  loggedIn: Boolean(state.session.currentUser)
+  loggedIn: Boolean(state.session.currentUser),
 })
 
-export default withRouter(connect(mapStateToProps, null)(Landing));
+const mapDispatchToProps = (dispatch) => ({
+  demoLogin: login,
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Landing));
