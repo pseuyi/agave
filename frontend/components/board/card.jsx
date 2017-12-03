40ec0ba769cf98919f084a87f6628f2fe573bbe5
pseuyi <pseuyi@gmail.com>
2017-12-03 02:22:04 -0500

    refactor props

--- frontend/components/board/card.jsx
+++ frontend/components/board/card.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-const Card = (props) => (
-  <article className={props.className} style={props.style}>
-    <header>{props.task.title}</header>
-    <p>{props.task.description}</p>
-    {props.task.children}
+const Card = ({ task, handleEditTaskModal, ...articleProps }) => (
+  <article {...articleProps}>
+    <header>{task.title}</header>
+    <p>{task.description}</p>
+    {task.children}
     <button
       className="edit-task-button non-draggable-element"
       type="edit"
-      onClick={() => props.handleEditTaskModal(props.task.id)}
+      onClick={() => handleEditTaskModal(task.id)}
       >
       edit
     </button>
