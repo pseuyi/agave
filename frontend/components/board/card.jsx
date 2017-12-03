dd23aa74839ad8c1424192f1fa0dff4c7ac880c1
pseuyi <pseuyi@gmail.com>
2017-12-03 02:23:42 -0500

    add comment

--- frontend/components/board/card.jsx
+++ frontend/components/board/card.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const Card = ({ task, handleEditTaskModal, ...articleProps }) => (
-  <article {...articleProps}>
+  <article {...articleProps}> // articleProps contains handlers from react-grid-layout that should be attached to this dom element
     <header>{task.title}</header>
     <p>{task.description}</p>
     {task.children}
