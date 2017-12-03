cef5f2e701f3aaf53ffb47534c82c33ea85ca976
pseuyi <pseuyi@gmail.com>
2017-12-03 02:04:31 -0500

    configure saga middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'
 import api from '../middleware/api';
 import rootReducer from '../reducers/root_reducer';
+import rootSaga from '../sagas/root_saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, api, logger)
+    applyMiddleware(thunk, api, logger, sagaMiddleware)
   )
 }
+
+sagaMiddleware.run(rootSaga);
 
 export default configureStore;
