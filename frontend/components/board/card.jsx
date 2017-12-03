facb58b3d6f4f90fd94fc676155962aad7e71199
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-03 16:31:31 -0500

    fix card for immutable

--- frontend/components/board/card.jsx
+++ frontend/components/board/card.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 // articleProps contains handlers from react-grid-layout that should be attached to this dom element
 const Card = ({ task, handleEditTaskModal, ...articleProps }) => (
   <article {...articleProps}>
-    <header>{task.title}</header>
-    <p>{task.description}</p>
-    {task.children}
+    <header>{task.get('title')}</header>
+    <p>{task.get('description')}</p>
+    {task.get('children')}
     <button
       className="edit-task-button non-draggable-element"
       type="edit"
-      onClick={() => handleEditTaskModal(task.id)}
+      onClick={() => handleEditTaskModal(task.get('id'))}
       >
       edit
     </button>
