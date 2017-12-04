c8843b151de86f7bcd46b601b30e8426ff34e712
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-04 15:55:39 -0500

    add immutable to users

--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -1,23 +1,21 @@
 import * as actions from '../consts/action-types';
+import { Map, Set } from 'immutable';
 
 const defaultState = {
-  usersByIds: {},
-  ids: []
-}
+  usersByIds: Map(),
+  ids: Set(),
+};
 
 const userReducer = (state = defaultState, action) => {
   switch (action.type) {
     case actions.RECEIVE_USER:
       return {
-        usersByIds: {
-          ...state.usersByIds,
-          ...action.payload.entities.users
-        },
-        ids: [...action.payload.result.users]
+        usersByIds: state.usersByIds.merge(action.payload.entities.users),
+        ids: state.ids.concat(action.payload.result.users),
       };
     default:
       return state;
   }
-}
+};
 
 export default userReducer;
